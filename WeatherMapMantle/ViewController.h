aa0b5f24e160d978d1acaebf32acb6ad4f3f6f24
sjcode <su.jian@me.com>
2015-09-02 14:22:13 +0800

    Initial Commit

---
+++ WeatherMapMantle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherMapMantle
+//
+//  Created by sujian on 15/9/2.
+//  Copyright (c) 2015年 sujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeatherMapMantle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeatherMapMantle
+//
+//  Created by sujian on 15/9/2.
+//  Copyright (c) 2015年 sujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
